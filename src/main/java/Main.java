bc68104272d5c70efdd63ce465a2918764c44054
Abraham Menyhart <menyharta@ipsystems.hu>
2017-07-25 12:19:32 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.net.URL;
+
+import com.google.gdata.client.*;
+import com.google.gdata.client.photos.*;
+import com.google.gdata.data.*;
+import com.google.gdata.data.media.*;
+import com.google.gdata.data.photos.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
